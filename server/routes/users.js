f62a96dfa5cf32c933ab731a1fa529f1bb506028
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2019-09-16 19:34:40 -0500

    MongoDB Static

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,95 +1,153 @@
 const express = require("express");
 const router = express.Router();
+const ObjectId = require("mongodb").ObjectID;
+const { Connection } = require("../mongo_config/Connection");
 
-const User = require("../models/user");
+const db = "printeardb";
+const collection = "users";
 
 /**
  * GET ALL
  */
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
   try {
-    const users =  await User.find();
-    res.json(users);
-  } catch(err) {
-    res.status(500).json({message: err.message})
-  }
-});
+    Connection.connectToMongo()
+      .then(async database => {
+        const client = database.db(db).collection(collection);
 
-/**
- * GET ONE
- */
-router.get("/:id", get_user, async (req, res) => {
-  res.json(res.user.name);
-});
-
-/**
- * POST
- */
-router.post("/", async (req, res) => {
-  try {
-    const new_user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      username: req.body.username
-    });
-    let saved_user = await new_user.save();
-    res.status(201).json(saved_user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-/**
- * PATCH - UPDATE
- */
-router.patch("/:id", get_user, async (req, res) => {
-  if (req.body.name) {
-    res.user.name = req.body.name;
-  }
-  if (req.body.email) {
-    res.user.email = req.body.email;
-  }
-  if (req.body.username) {
-    res.user.username = req.body.username;
-  }
-  try {
-    const updated_user = await res.user.save();
-    res.json(updated_user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-/**
- * DELETE
- */
-router.delete("/:id", get_user, async (req, res) => {
-  try {
-    await res.user.remove();
-    res.json({ message: "User Deleted" });
+        await client
+          .find({})
+          .toArray()
+          .then(x => res.status(200).json(x))
+          .catch(err => res.status(500).json({ message: err.message }));
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
 /**
- * Middleware
+ * GET ONE
  */
-async function get_user(req, res, next) {
-  let user;
+router.get("/:id", (req, res) => {
   try {
-    user = await User.findById(req.params.id);
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: `User ${req.params.id} does not exist` });
-    }
+    Connection.connectToMongo()
+      .then(database => {
+        const client = database.db(db).collection(collection);
+
+        client
+          .find({ _id: ObjectId(req.params.id) })
+          .toArray()
+          .then(x => res.status(200).json(x))
+          .catch(err => res.status(404).json({ message: err.message }));
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
   } catch (err) {
-    return res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
-  res.user = user;
-  next();
-}
+});
+
+/**
+ * POST
+ */
+router.post("/", (req, res) => {
+  const new_user = {
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email
+  };
+
+  try {
+    Connection.connectToMongo()
+      .then(database => {
+        const client = database.db(db).collection(collection);
+
+        client.insertOne(new_user, (err, result) => {
+          if (err) {
+            res.status(400).json({ message: err.message });
+            return;
+          }
+          res.status(201).send(result.ops);
+        });
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * PATCH
+ */
+router.patch("/:id", (req, res) => {
+  let updating = {};
+  if (req.body.email) {
+    updating.email = req.body.email;
+  }
+  if (req.body.name) {
+    updating.name = req.body.name;
+  }
+  if (req.body.username) {
+    updating.username = req.body.usernmae;
+  }
+
+  try {
+    Connection.connectToMongo()
+      .then(database => {
+        const client = database.db(db).collection(collection);
+
+        client.findOneAndUpdate(
+          { _id: ObjectId(req.params.id) },
+          { $set: updating },
+          { returnOriginal: false },
+          (err, result) => {
+            if (err) {
+              res.status(400).json({ message: err.message });
+              return;
+            }
+            res.status(200).json(result.value);
+          }
+        );
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * DELETE
+ */
+router.delete("/:id", (req, res) => {
+  try {
+    Connection.connectToMongo()
+      .then(database => {
+        const client = database.db(db).collection(collection);
+
+        client.findOneAndDelete({ _id: ObjectId(req.params.id) }, (err, result) => {
+          if (err) {
+            res.status(404).json({ message: err.message });
+            return;
+          }
+          res.status(200).send(result.value);
+        });
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 /**
  * ? Export
--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
+require("dotenv").config();
+
 const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+/**
+ * Routers
+ */
 const users_route = require("./routes/users");
 
 var app = express();
 const PORT = normalizePort(process.env.PORT || "5000");
-
-require("dotenv").config();
 
 app
   .use(express.json())
@@ -19,33 +21,20 @@ app
   .use(cookieParser())
 
 /**
- * MongoDB Connection
- */
-mongoose.connect(process.env.MLAB, { useNewUrlParser: true, useCreateIndex: true })
-        .catch(err => console.log(" \n\n------- \nDB Auth Error\nEn .env poner MLAB=mongodb://<dbuser>:<dbpassword>@ds123500.mlab.com:23500/printeardb\nCambiando los valores de <dbuser> y <dbpassword>\n ----------- \n\n", err))
-const db = mongoose.connection
-db.on('error', (err) => console.log('DB Error :: ', err));
-db.once('open', _ => console.log('DB Connection Established.'));
- 
-/**
  * Routes
  */
 app.use("/api/users", users_route);
 
-// Front - React App
 app
   .use(express.static(path.join(__dirname, "../client/build")))
   .get("/", (req, res) => res.sendFile(path.join(__dirname,'../client/build/index.html')));
 
-// 404
-app.get("*", (req, res) => res.status(404).json({message: '404 not found'}));
 
 
 /**
  * Listen
  */
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 
 /**
  * Get port from environment and store in Express.
