7e0c9584b18b2c182cd88dc1153966cd939e4a56
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-09-22 10:12:35 -0500

    Cambio porque faltaba un slash al importar el route de servicios.

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app
  */
 app.use("/api/usuarios", usuarios_route);
 app.use("/api/pedidos", pedidos_route);
-app.use("api/servicios", servicios_route);
+app.use("/api/servicios", servicios_route);
 
 app
   .use(express.static(path.join(__dirname, "../client/build")))
